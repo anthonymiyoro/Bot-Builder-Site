296dcb78d7f40471835557464c201adc9033d074
anthonymiyoro <amiyoro@gmail.com>
2016-02-07 19:32:44 +0300

    Added points assigning to different users

--- src/questions/matching.py
+++ src/questions/matching.py
@@ -18,11 +18,18 @@ def get_match(user_a, user_b):
         user_b_answer = user_b_answers.my_answer
         user_b_pref = user_b_answers.their_answer
 
+        user_a_total_awarded = 0
+        user_b_total_awarded = 0
+
+
         if user_a_answer == user_b_pref:
+            user_b_total_awarded += user_a_answers.their_points
             print "%s fits with %s's preference" %(user_a_answers.user.username, user_a_answers.user.username)
 
         if user_a_pref == user_b_answer:
+            user_a_total_awarded += user_a_answers.their_points
             print "%s fits with user %s's preference"  %(user_a_answers.user.username, user_b_answers.user.username)
 
-        if user_a_answer == user_b_pref && user_a_pref == user_b_answer:
+        if user_a_answer == user_b_pref and user_a_pref == user_b_answer:
             print "this is an ideal answer for both"
+
