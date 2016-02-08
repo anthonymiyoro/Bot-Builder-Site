d388c3d909fb0afcc098aab2710f1aa758d1317a
anthonymiyoro <amiyoro@gmail.com>
2016-02-08 15:35:08 +0300

    Added a model match manager

--- src/matches/models.py
+++ src/matches/models.py
@@ -4,6 +4,28 @@ from django.db import models
 
 # Create your models here.
 
+class MatchManager(models.Manager):
+    def get_or_create_match(self,  user_a=None, user_b=None):
+        try:
+            obj = self.get(user_a=user_a, user_b=user_b)
+        except:
+            obj = None
+        try:
+            obj_2 = self.get(user_a=user_b, userb=user_a)
+        except:
+            obj_2 = None
+        if obj and not obj_2:
+            return obj, False
+        elif not obj and obj_2:
+            return obj_2, False
+        else:
+            new_instance = self.create(user_a=user_a, user_b=user_b)
+            new_instance.match_decimal = 0.85
+            new_instance.questions_answered = 20
+            new_instance.save()
+            return new_instance, True
+
+
 class Match(models.Model):
     user_a = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='match_user_a')
     user_b = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='match_user_b')
@@ -13,4 +35,5 @@ class Match(models.Model):
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __unicode__(self):
-        return self.match_decimal
+        return self.match_decimal = 0.85
+
