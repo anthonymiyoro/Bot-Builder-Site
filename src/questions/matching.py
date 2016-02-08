5600b4b06087772893ed68d126e358a5f9f5095b
anthonymiyoro <amiyoro@gmail.com>
2016-02-08 11:47:01 +0300

    Created match percentage system

--- src/questions/matching.py
+++ src/questions/matching.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.contrib.auth import get_user_model
 
 from questions.models import UserAnswer
@@ -8,43 +9,30 @@ users = User.objects.all
 all_user_answers = UserAnswer.objects.all().order_by("user__id")
 
 
-def get_match(user_a, user_b):
-    user_a_answers = UserAnswer.objects.filter(user=user_a)[0]
-    user_b_answers = UserAnswer.objects.filter(user=user_b)[0]
-
-    if user_a_answers.question.id == user_b_answers.question.id:
-        user_a_answer = user_a_answers.my_answer
-        user_a_pref = user_a_answers.their_answer
-        user_b_answer = user_b_answers.my_answer
-        user_b_pref = user_b_answers.their_answer
-
-        user_a_total_awarded = 0
-        user_b_total_awarded = 0
-
-
-        if user_a_answer == user_b_pref:
-            user_b_total_awarded += user_a_answers.their_points
-            print "%s fits with %s's preference" %(user_a_answers.user.username, user_a_answers.user.username)
-
-        if user_a_pref == user_b_answer:
-            user_a_total_awarded += user_a_answers.their_points
-            print "%s fits with user %s's preference"  %(user_a_answers.user.username, user_b_answers.user.username)
-
-        if user_a_answer == user_b_pref and user_a_pref == user_b_answer:
-            print "this is an ideal answer for both"
-
-
 def get_points(user_a, user_b):
-    a_answers = UserAnswer.objects.filter(user=user_a)[0]
-    b_answers = UserAnswer.objects.filter(user=user_b)[0]
+    a_answers = UserAnswer.objects.filter(user=user_a)
+    b_answers = UserAnswer.objects.filter(user=user_b)
     a_total_awarded = 0
     a_points_possible = 0
-    if a_answers.question.id == b_answers.question.id:
-        a_pref = a_answers.their_answer
-        b_answer = b_answers.my_answer
-        if a_pref == b_answer:
-            # points are awarded for a correct answer
-            a_total_awarded += a_answers.their_points
-        # total points are then assigned
-        a_points_possible += a_answers.their_points
-    print "%s has awarded %s points to %s" %(user_a, a_total_awarded, user_b)
+    num_question = 0
+    for a in a_answers:
+        for b in b_answers:
+            if a.question.id == b.question.id:
+                num_question += 1
+                a_pref = a.their_answer
+                b_answer = b.my_answer
+                if a_pref == b_answer:
+                    # points are awarded for a correct answer
+                    a_total_awarded += a.their_points
+                # total points are then assigned
+                a_points_possible += a.their_points
+            print "%s has awarded %s points to %s" %(user_a, a_total_awarded, a_points_possible, user_b)
+            percent = a_total_awarded / Decimal(a_points_possible)
+            print percent, num_question
+            if percent == 0:
+                percent = 0.000001
+            return percent, num_question
+
+
+match_percentage = "%.2f" % ((Decimal(a[0]) * Decimal(b[0])) ** (1/Decimal(b[1])))
+print match_percentage
