c91b2da1d9577b54a39754c962288011c7bae699
anthonymiyoro <amiyoro@gmail.com>
2016-02-13 18:07:32 +0300

    Installed profile app and integrated it to the admin

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -5,12 +5,17 @@ from django.db import models
 
 User = settings.AUTH_USER_MODEL
 
-def upload_location(instance, filename):
 
+def upload_location(instance, filename):
+    extension = filename.split(".")[1]
     location = str(instance.user.username)
-    return "%s/%s" %(location)
+    return "%s/%s" % (location, extension)
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User)
     location = models.CharField(max_length=120, null=True, blank=True)
     picture = models.ImageField(upload_to=upload_location, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.user.username
