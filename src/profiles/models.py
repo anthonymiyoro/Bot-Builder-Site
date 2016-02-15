e5ca615113ab0de732ae455607d310daa324ae8e
anthonymiyoro <amiyoro@gmail.com>
2016-02-15 07:52:44 +0300

    Fixed useranswer integrity errors

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -8,9 +8,9 @@ User = settings.AUTH_USER_MODEL
 
 
 def upload_location(instance, filename):
-    extension = filename.split(".")[1]
+    # extension = filename.split(".")[1]
     location = str(instance.user.username)
-    return "%s/%s" % (location, extension)
+    return "%s/%s" % (location, filename)
 
 
 class Profile(models.Model):
@@ -18,9 +18,10 @@ class Profile(models.Model):
     location = models.CharField(max_length=120, null=True, blank=True)
     picture = models.ImageField(upload_to=upload_location, null=True, blank=True)
 
-    def __unicode__(self):
+    def __unicode__(self):  # __str__(self):
         return self.user.username
 
     def get_absolute_url(self):
+        # url = reverse("question_single", kwargs={"id": self.id})
         url = reverse("profile", kwargs={"username": self.user.username})
         return url
--- src/questions/models.py
+++ src/questions/models.py
@@ -1,3 +1,4 @@
+
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save, pre_save
@@ -12,18 +13,20 @@ class Question(models.Model):
     draft = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
-    def __unicode__(self):
+    # answers = models.ManyToManyField('Answer')
+
+    def __unicode__(self):  # def __str__(self):
         return self.text[:10]
 
 
 class Answer(models.Model):
     question = models.ForeignKey(Question)
-    text = models.TextField(max_length=120)
+    text = models.CharField(max_length=120)
     active = models.BooleanField(default=True)
     draft = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
-    def __unicode__(self):
+    def __unicode__(self):  # def __str__(self):
         return self.text[:10]
 
 
@@ -39,17 +42,16 @@ class UserAnswer(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     question = models.ForeignKey(Question)
     my_answer = models.ForeignKey(Answer, related_name='user_answer')
-    my_answer_importance = models.CharField(max_length=55, choices=LEVELS)
+    my_answer_importance = models.CharField(max_length=50, choices=LEVELS)
     my_points = models.IntegerField(default=-1)
     # the answer field is allowed to be blank
     their_answer = models.ForeignKey(Answer, null=True, blank=True, related_name='match_answer')
-    their_importance = models.CharField(max_length=55, choices=LEVELS)
+    their_importance = models.CharField(max_length=50, choices=LEVELS)
     their_points = models.IntegerField(default=-1)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
     def __unicode__(self):
         return self.my_answer.text[:10]
-
 
 # list of scores per importance
 def score_importance(importance_level):
@@ -63,17 +65,12 @@ def score_importance(importance_level):
         points = 0
     else:
         points = 0
+    return points
 
 
 @receiver(pre_save, sender=UserAnswer)
-def update_user_answer_score(sender, instance,  *args, **kwargs):
+def update_user_answer_score(sender, instance, *args, **kwargs):
     my_points = score_importance(instance.my_answer_importance)
     instance.my_points = my_points
     their_points = score_importance(instance.their_importance)
     instance.their_points = their_points
-
-
-pre_save.connect(update_user_answer_score, sender=UserAnswer)
-
-
-
